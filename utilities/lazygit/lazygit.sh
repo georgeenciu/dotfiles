06e1a676c31b577259e8c45d85443c6773f4c288
George Enciu <george.enciu@gmail.com>
2023-01-16 21:15:06 +0100

    fix lazygit

--- utilities/lazygit/lazygit.sh
+++ utilities/lazygit/lazygit.sh
@@ -8,7 +8,7 @@ install_lazygit() {
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
-    mkdir ~/.config/lazygit
-    ln -sfn ~/code/georgeenciu/dotfiles/utilities/lazygit/lazygit_config.yml ~/.config/lazygit/config.yml 
+    sudo mkdir ~/.config/lazygit
+    sudo ln -sfn ~/code/georgeenciu/dotfiles/utilities/lazygit/lazygit_config.yml ~/.config/lazygit/config.yml 
   fi
 }

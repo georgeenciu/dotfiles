30262b39b8f95c97491249866ad96fa9ebae01b2
George Enciu <george.enciu@gmail.com>
2023-02-14 20:01:58 +0100

    move to lazy.nvim

--- editors/nvim/lua/plugins/init.lua
+++ editors/nvim/lua/plugins/init.lua
@@ -21,4 +21,18 @@ return {
       colorscheme = "onedark",
     },
   },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
 }

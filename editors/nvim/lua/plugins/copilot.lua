5f1356deb3ab06e9bd1ea4863b0d6b02a067ad1a
George Enciu <george.enciu@gmail.com>
2023-03-10 23:10:32 +0100

    copilot update

--- editors/nvim/lua/plugins/copilot.lua
+++ editors/nvim/lua/plugins/copilot.lua
@@ -1,16 +1,25 @@
 return {
   {
-    "github/copilot.vim",
-    init = function()
-      vim.g.copilot_no_tab_map = true
-      vim.keymap.set(
-        "i",
-        "<Plug>(vimrc:copilot-dummy-map)",
-        'copilot#Accept("")',
-        { silent = true, expr = true, desc = "Copilot dummy accept" }
-      )
-      -- local map = vim.keymap.set
-      -- map("i", "<C-j>", "copilot#Accept('<CR>')", { noremap = true, silent = true, expr = true })
-    end,
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+    --   init = function()
+    --     vim.g.copilot_no_tab_map = true
+    --     vim.keymap.set(
+    --       "i",
+    --       "<Plug>(vimrc:copilot-dummy-map)",
+    --       'copilot#Accept("")',
+    --       { silent = true, expr = true, desc = "Copilot dummy accept" }
+    --     )
+    --     -- local map = vim.keymap.set
+    --     -- map("i", "<C-j>", "copilot#Accept('<CR>')", { noremap = true, silent = true, expr = true })
+    --   end,
+    -- },
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    opts = {},
   },
 }

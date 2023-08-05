157ad6a43b68dfe0579a91229bde2516e7f5d91a
George Enciu <george.enciu@gmail.com>
2023-08-05 20:29:17 +0200

    nordic theme

--- editors/nvim/lua/config/lazy.lua
+++ editors/nvim/lua/config/lazy.lua
@@ -15,7 +15,10 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "plugins" },
   },
   defaults = {
@@ -27,7 +30,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "onedark", "tokyonight", "habamax" } },
+  install = { colorscheme = { "nordic" } },
   checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {

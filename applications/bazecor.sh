6a82f5c531d3f8626fde426c714df9efd5676f4f
George Enciu <george.enciu@gmail.com>
2023-08-31 20:41:49 +0200

    bazecor update

--- applications/bazecor.sh
+++ applications/bazecor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-install_bazecor () {
-  if exists "Bazecor"; then 
+install_bazecor() {
+	if exists "Bazecor"; then
 		echo_item "Bazecor is already installed" "green"
-  else
+	else
 		echo_item "installing Bazecor" "green"
-    wget -O Bazecor https://github.com/Dygmalab/Bazecor/releases/download/bazecor-1.0.0/Bazecor-1.0.0.AppImage
-    chmod u+x Bazecor
-    sudo mv Bazecor /usr/bin/Bazecor
-  fi
+		wget -O Bazecor https://github.com/Dygmalab/Bazecor/releases/download/bazecor-1.3.2/Bazecor-1.3.2.AppImage
+		chmod u+x Bazecor
+		sudo mv Bazecor /usr/bin/Bazecor
+	fi
 }

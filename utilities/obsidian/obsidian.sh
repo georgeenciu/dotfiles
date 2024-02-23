96108ae86be661eda6431250073f797c952adcdf
George Enciu <george.enciu@gmail.com>
2024-02-23 11:18:46 +0100

    update obsidian

--- utilities/obsidian/obsidian.sh
+++ utilities/obsidian/obsidian.sh
@@ -2,11 +2,11 @@
 
 install_obsidian() {
 	if exists "Obsidian"; then
-		echo_item "neovim is already installed" "green"
+		echo_item "Obsidian is already installed" "green"
 	else
-		echo_item "installing neovim" "green"
-		curl -LO https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/Obsidian-1.4.16.AppImage
-		chmod u+x Obsidian-1.4.16.AppImage
-		sudo mv Obsidian-1.4.16.AppImage /usr/bin/Obsidian
+		echo_item "installing Obsidian" "green"
+		wget -O Obsidian https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.8/Obsidian-1.5.8.AppImage
+		chmod u+x Obsidian
+		sudo mv Obsidian /usr/bin/Obsidian
 	fi
 }
